d9c7072a9e8073ad58de17fc62d041bcc705b236
Aurora Hampton <argh.hay@gmail.com>
2021-03-19 11:51:59 -0700

    new relic

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,5 +1,8 @@
-class MessagesChannel < ApplicationCable::Channel  
+
+class MessagesChannel < ApplicationCable::Channel
+  include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation
   def subscribed
-    stream_from 'messages'
+      stream_from 'messages'
   end
+  add_transaction_tracer :subscribed, :category => :task
 end  
